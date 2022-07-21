5f985cfd7a970bddc2e00184cf4adab98e8c6e2d
LesleyBonyo <lesleybonyo@gmail.com>
2022-07-21 17:58:44 +0300

    prime numbers

---
+++ prime.py
@@ -0,0 +1,15 @@
+def prime(num):
+    flag = False
+
+    for i in range(2, num):
+        if num % i == 0:
+            flag = True
+            break
+
+    if flag:
+        print(num, " is not a prime number")
+    else:
+        print(num, " is a prime number")
+
+
+prime(2)
